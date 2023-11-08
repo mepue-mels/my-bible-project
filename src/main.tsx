e63cbc047cb7b42efbe436eca36ad042270d7802
MJ Ronduen <rjhronduen@mymail.mapua.edu.ph>
2023-11-08 20:16:09 +0800

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,6 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Register />
+    <About />
   </React.StrictMode>,
 )
